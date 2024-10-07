48f50781d33b7dd2544bc8ac5bbc3a4195d517af
shubhambendkhale77 <shubhambendkhale77@gmail.com>
2024-10-07 13:26:37 +0530

    even or odd number program

---
+++ index.js
@@ -0,0 +1,14 @@
+const numbers = [
+    3, 7, 2, 15, 9, 20, 5, 14, 12, 1, 8, 11, 6, 19, 4, 10, 17, 13, 16, 18,
+  ];
+
+  for(i=0;i<=numbers.length;i++){
+    if(numbers[i] %2===0){
+        console.log("Even",i)
+    }
+  }
+  for(i=0;i<=numbers.length;i++){
+    if(numbers[i] %2!==0){
+        console.log("odd",i)
+    }
+  }
